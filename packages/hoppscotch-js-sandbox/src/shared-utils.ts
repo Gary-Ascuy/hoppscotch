e9c9973b264c90378b9e0a1702deb9fe79662973
Gary Ascuy <gary.ascuy@gmail.com>
2025-03-30 19:46:35 -0500

    feat: add implementation of isEqual method

--- packages/hoppscotch-js-sandbox/src/shared-utils.ts
+++ packages/hoppscotch-js-sandbox/src/shared-utils.ts
@@ -229,7 +229,32 @@ export function preventCyclicObjects(
   }
 }
 
-export const isDeepEqual = (_a: unknown, _b: unknown): boolean => {
+export const isObject = (object: unknown): boolean => {
+  return object != null && typeof object === "object"
+}
+
+export const isDeepEqual = (object1: any, object2: any): boolean => {
+  const objKeys1 = Object.keys(object1)
+  const objKeys2 = Object.keys(object2)
+
+  if (objKeys1.length !== objKeys2.length) {
+    return false
+  }
+
+  for (const key of objKeys1) {
+    const value1 = object1[key]
+    const value2 = object2[key]
+
+    const isObjects = isObject(value1) && isObject(value2)
+
+    if (
+      (isObjects && !isDeepEqual(value1, value2)) ||
+      (!isObjects && value1 !== value2)
+    ) {
+      return false
+    }
+  }
+
   return true
 }
 
